f39e040df3ac1726f22a3379c7418d5ab8a5ef57
Breno Zeymer <bzeymer@fitec.org.br>
2018-08-01 20:08:15 -0300

    Create Angular project and mounts basic structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ChatComponent } from './chat/chat.component';
+import { UsersComponent } from './chat/users/users.component';
+import { MessagesComponent } from './chat/messages/messages.component';
+import { CommandsComponent } from './chat/commands/commands.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ChatComponent,
+    UsersComponent,
+    MessagesComponent,
+    CommandsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
